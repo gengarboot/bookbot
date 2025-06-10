7291dc21d150af25170951e39b8ac8cd8c117cb8
gengarboot <gengarboot@fastmail.com>
2025-06-10 20:05:53 +0200

    adding wordcount

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+def main():
+	text = get_book_text("./books/frankenstein.txt")
+	print(text)
+	num_of_words(text)
+	print(f"{num_of_words(text)} words found in the document")
+
+def num_of_words(text):
+	word_count = text.split()
+	return len(word_count)
+
 def get_book_text(filepath):
 	with open(filepath) as f:
 		return f.read()
-
-def main():
-	print(get_book_text("./books/frankenstein.txt"))
 
 main()
