ae7e4e9ea4edde110beb821e2a685e61d2bdf750
gengarboot <gengarboot@fastmail.com>
2025-06-11 18:01:27 +0200

    splitting out get_num_words to its own file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import get_num_words
+
 def main():
 	text = get_book_text("./books/frankenstein.txt")
 	print(text)
-	num_of_words(text)
-	print(f"{num_of_words(text)} words found in the document")
-
-def num_of_words(text):
-	word_count = text.split()
-	return len(word_count)
+	get_num_words(text)
+	print(f"{get_num_words(text)} words found in the document")
 
 def get_book_text(filepath):
 	with open(filepath) as f:
