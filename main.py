7ea852c739dc1132411adecaccee36f161ea56e1
gengarboot <gengarboot@fastmail.com>
2025-06-11 19:10:59 +0200

    cleaning up and removing duplication

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import sys
-from stats import get_num_words
-from stats import get_num_characters
-from stats import sort_dictionary
+from stats import (
+	get_num_words,
+	get_num_characters,
+	sort_dictionary,
+)
 
 def main():
 	if len(sys.argv) != 2:
 		print("Usage: python3 main.py <path_to_book>")
 		sys.exit(1)
+	book_path = sys.argv[1]
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+		return f.read()
+
 	print("============ BOOKBOT ============")
-	print(f"Analyzing book found at {sys.argv[1]}...")
-	text = get_book_text(f"./{sys.argv[1]}")
+	print(f"Analyzing book found at {book_path}...")
+	text = get_book_text(f"./{book_path}")
 	print("----------- Word Count ----------")
 	get_num_words(text)
 	print(f"Found {get_num_words(text)} total words")
@@ -18,10 +26,7 @@ def main():
 	for item in result:
 		if item['char'].isalpha():
 			print(f"{item['char']}: {item['num']}")
-	print("============= END ===============")
 
-def get_book_text(filepath):
-	with open(filepath) as f:
-		return f.read()
+	print("============= END ===============")
 
 main()
