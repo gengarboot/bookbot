e2d870c2064c1ea928d8f48c28a7654c3e6c5f7c
gengarboot <gengarboot@fastmail.com>
2025-06-11 18:57:44 +0200

    Do word count

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import sort_dictionary
 
 def main():
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
 	text = get_book_text("./books/frankenstein.txt")
-	print(text)
+	# print(text)
+	print("----------- Word Count ----------")
 	get_num_words(text)
-	print(f"{get_num_words(text)} words found in the document")
-	print(f"{get_num_characters(text)}")
+	print(f"Found {get_num_words(text)} total words")
+	print("--------- Character Count -------")
+	result = sort_dictionary(get_num_characters(text))
+	for item in result:
+		if item['char'].isalpha():
+			print(f"{item['char']}: {item['num']}")
+	print("============= END ===============")
 
 def get_book_text(filepath):
 	with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -10,5 +10,24 @@ def get_num_characters(text):
 			dict[char] = 1
 		else:
 			dict[char] +=1
-			
+
 	return dict
+
+def sort_on(dict):
+	return dict["num"]
+
+def sort_dictionary(dictionary):
+	sorted_items = []	
+
+	# dictionary.items gives a list of touples where each index in touple is key / value
+	for item in dictionary.items():
+		new_dictionary = {}
+		new_dictionary["char"] = item[0]
+		new_dictionary["num"] = item[1]
+		sorted_items.append(new_dictionary)
+
+	sorted_items.sort(reverse=True, key=sort_on)
+	return sorted_items
+	
+
+	
