6bab4b31009112858f2478e335c8e3f1d855c830
gengarboot <gengarboot@fastmail.com>
2025-06-11 19:01:17 +0200

    refactor after solution file... much smarter...

--- stats.py
+++ stats.py
@@ -20,12 +20,9 @@ def sort_dictionary(dictionary):
 	sorted_items = []	
 
 	# dictionary.items gives a list of touples where each index in touple is key / value
-	for item in dictionary.items():
-		new_dictionary = {}
-		new_dictionary["char"] = item[0]
-		new_dictionary["num"] = item[1]
-		sorted_items.append(new_dictionary)
-
+	for key in dictionary:
+		sorted_items.append({"char": key, "num": dictionary[key]})
+		
 	sorted_items.sort(reverse=True, key=sort_on)
 	return sorted_items
 	
